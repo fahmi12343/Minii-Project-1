996ea4665ec432abed66f255981a7000c9e6eea9
Lukaririnki <nurfahmiazis19@gmail.com>
2021-07-21 20:48:12 +0700

    firs commit

--- lib/features/dashboard/presentation/pages/dummy.dart
+++ lib/features/dashboard/presentation/pages/dummy.dart
@@ -299,9 +299,6 @@ class Dummy extends StatelessWidget {
                             ))
                         .toList(),
                   ),
-                  Expanded(child: Container(
-
-                  )),
                 ],
               ),
             ),
